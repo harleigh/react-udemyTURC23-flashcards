72177f60735604304ff6ec7f53af68f36e98b76c
harleigh <harleigh.marsh@gmail.com>
2023-09-28 11:20:18 -0700

    flashcard displays answer when clicked; turns back to question form if clicked in answer form

--- src/Components/Flashcard.js
+++ src/Components/Flashcard.js
@@ -1,13 +1,19 @@
-export function Flashcard({ isFlipped, question, answer, id }) {
+//
+//an individual flashcard is responsible for notifying its parent when this flashcard is clicked
+//   The parent component tells how this component should behave when clicked
+export function Flashcard({ onClick, showAnswer, question, answer }) {
   const setClassName = () => {
-    return isFlipped ? "selectedCard" : "flashcard";
+    return showAnswer ? "selectedCard" : "flashcard";
   };
   const setDisplay = () => {
-    return isFlipped ? answer : question;
+    return showAnswer ? answer : question;
   };
+  //this flashcard was selected so we will flip this card on the refresh
   return (
     <>
-      <div className={setClassName()}>{setDisplay()}</div>
+      <div className={setClassName()} onClick={onClick}>
+        {setDisplay()}
+      </div>
     </>
   );
 }
--- src/Components/FlashcardApp.js
+++ src/Components/FlashcardApp.js
@@ -1,20 +1,36 @@
 /* Project based on "steps" project from TURC23 */
+
+//fun difference: This is done through components, and I tell the custom flashcard
+//component how to behave when an on-click event occurs :)
+
 import { Flashcard } from "./Flashcard";
 import { flashcardData } from "../data";
 import { useState } from "react";
 
+//Purpose of the Flashcard app is to display all of the flashcards on the screen,
+//and to show the correct side of the flashcard (whether question side or answer side)
+//based on if the user selected said card (only one answer is shown at any time)
 export default function FlashcardApp() {
-  const [selectedCard, setSelectedCard] = useState(null);
+  const [selectedCardId, setSelectedCardId] = useState(null);
+
+  //how the flashcard is to behave when clicked
+  const behaviourOnClick = (id) => {
+    if (id === selectedCardId) {
+      setSelectedCardId(null);
+    } else {
+      setSelectedCardId(id);
+    }
+  };
 
   const buildFlashcards = () => {
-    return flashcardData.map((card) => {
+    return flashcardData.map((curCard) => {
       return (
         <Flashcard
-          isFlipped={false}
-          question={card.question}
-          answer={card.answer}
-          id={card.id}
-          key={card.id}
+          onClick={() => behaviourOnClick(curCard.id)}
+          showAnswer={selectedCardId === curCard.id}
+          question={curCard.question}
+          answer={curCard.answer}
+          key={curCard.id}
         />
       );
     });
